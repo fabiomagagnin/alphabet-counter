b35a8770955cb59fe5520f602438293fc4665c5b
Fábio Magagnin <fabio.magagnin@meta.com.br>
2021-10-16 14:47:05 -0300

    Making index use an input param

--- index.mjs
+++ index.mjs
@@ -1,7 +1,21 @@
 import Digit from "./digit.mjs";
 
-const digit = new Digit()
-console.log(digit.print())
-for (let i = 2; i <= 20; i++) digit.next()
-console.log(digit.print())
+const quantity = Number(process.argv[2])
 
+if (isNaN(quantity) || !Number.isInteger(quantity) || quantity < 1 ) {
+    console.log(`'${process.argv[2]}' should be an integer bigger then zero!`)
+    process.exit(1)
+}
+
+console.log(`Printing ${quantity} value(s)...`)
+
+const print = digit => {
+    process.stdout.write(digit.printAllBrothers() + ' ')
+}
+
+const digit = new Digit()
+print(digit)
+for (let i = 2; i <= quantity; i++) {
+    digit.next()
+    print(digit)
+}
